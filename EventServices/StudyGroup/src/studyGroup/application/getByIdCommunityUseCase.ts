60f6474e42b0c623c5b8ad8beca39fca7fbdcd34
Erick201424 <98076338+Erick201424@users.noreply.github.com>
2023-12-08 08:27:51 -0600

    Microservicies update

--- EventServices/StudyGroup/src/studyGroup/application/getByIdCommunityUseCase.ts
+++ EventServices/StudyGroup/src/studyGroup/application/getByIdCommunityUseCase.ts
@@ -20,36 +20,7 @@ export class GetByIdCommunityUseCase {
             // Obtener datos de la comunidad desde el repositorio
             const getCommunity = await this.CommunityRepository.getByIdCommunity(id);
 
-            // Verificar si getCommunity es un array y tiene al menos un elemento
-            if (!Array.isArray(getCommunity) || getCommunity.length === 0) {
-                return null;
-            }
-
-            // Obtener el id del usuario creador de la comunidad
-            const userId = (getCommunity[0] as Community).iduser; // Accede al primer elemento del array
-
-            console.log("userID: ", userId);
-            // Hacer una petición GET al microservicio de estudiantes
-            const studentResponse = await axios.get(`http://localhost:3002/user/${userId}`);
-
-            // Verificar si la petición al microservicio fue exitosa
-            if (studentResponse.status) {
-                const studentData = studentResponse.data;
-
-                // Agregar la información del estudiante a los datos de la comunidad
-                const communityWithStudent: CommunityWithStudent = {
-                    ...getCommunity,
-                    studentData: studentData,  // Ajusta esto según la estructura de tu microservicio
-                };
-
-                console.log(communityWithStudent);
-
-                return communityWithStudent;
-            } else {
-                // Manejar errores de la petición al microservicio de estudiantes
-                console.error('Error al obtener datos del estudiante:', studentResponse.statusText);
-                return getCommunity;  // Devuelve la comunidad sin datos del estudiante en caso de error
-            }
+            return getCommunity;
         } catch (error) {
             return null;
         }
